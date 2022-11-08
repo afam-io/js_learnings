673d04ee5438df544d3337c548e3cf5725051d69
AfamOkoh <afamokoh@gmail.com>
2022-11-08 22:15:28 +0000

    start on squares in a rectangele codewars problem

---
+++ squares_in_a_rectangle.js
@@ -0,0 +1,13 @@
+const perimeter = (n) => {
+  let arr = [];
+  for (let i = 0; (i = n); i++) {}
+
+  return 5;
+};
+
+console.log(perimeter());
+
+/* 
+  Given a fibonanacci sequence the perimeter will always be 4 times the sequence of fibonacci numbers
+
+  */
