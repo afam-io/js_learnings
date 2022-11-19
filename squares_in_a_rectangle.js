519c61fcc4599e2e0e2047aa4a2fa6da1ba202ff
AfamOkoh <afamokoh@gmail.com>
2022-11-19 22:46:57 +0000

    chore: remove console logs

--- squares_in_a_rectangle.js
+++ squares_in_a_rectangle.js
@@ -3,18 +3,11 @@ const perimeter = (n) => {
   if (n === 0) {
     return 4;
   }
-
   let count = 1;
   for (let i = n - 2; i >= 0; i--) {
     count++;
-
     fibSeqArr.push(fibSeqArr[count - 2] + fibSeqArr[count - 1]);
   }
   console.log({ fibSeqArr });
   return fibSeqArr.reduce((a, b) => a + b) * 4;
 };
-
-console.log({ perim0: perimeter(0) });
-console.log({ perim5: perimeter(5) });
-console.log({ perim7: perimeter(7) });
-console.log({ perim20: perimeter(20) });
