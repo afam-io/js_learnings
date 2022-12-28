f15c2839f4ee5a534f4cfbc5b1b5a552c873acfd
AfamOkoh <afamokoh@gmail.com>
2022-12-28 21:06:27 +0000

    test: add first test plus jest config.

---
+++ codewars/meeting.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, test } from '@jest/globals';
+import meeting from './meeting';
+
+describe('meeting function', () => {
+  const s1 =
+    'Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill';
+  const a1 =
+    '(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)';
+  test('friends list one returns correctly sorted string', () => {
+    expect(meeting(s1)).toBe(a1);
+  });
+});
